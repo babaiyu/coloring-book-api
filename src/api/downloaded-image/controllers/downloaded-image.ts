77ee86a28884eefcd908c233fea0e3b01c9bf0b4
babaiyu <bayuputra9a@gmail.com>
2024-12-06 19:29:58 +0700

    [#FEATURES]: Implement cache for download image

---
+++ src/api/downloaded-image/controllers/downloaded-image.ts
@@ -0,0 +1,7 @@
+/**
+ * downloaded-image controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::downloaded-image.downloaded-image');
---
+++ src/api/downloaded-image/routes/downloaded-image.ts
@@ -0,0 +1,7 @@
+/**
+ * downloaded-image router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::downloaded-image.downloaded-image');
